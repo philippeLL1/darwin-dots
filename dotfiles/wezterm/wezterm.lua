934003a81418269ba96626cb02f67d91edf5c19f
Philippe Latour <drawer@darwin-devel.local>
2023-06-15 12:51:23 -0400

    Wezterm tab bar style moved to separate file

--- dotfiles/wezterm/wezterm.lua
+++ dotfiles/wezterm/wezterm.lua
@@ -1,4 +1,5 @@
 -- Pull in the wezterm API
+require"tabStyle"
 local wezterm = require 'wezterm'
 
 -- This table will hold the configuration.
@@ -9,13 +10,12 @@ local config = {}
 if wezterm.config_builder then
   config = wezterm.config_builder()
 end
--- config options
-config.hide_tab_bar_if_only_one_tab = true
-config.tab_bar_at_bottom = false
+
+-- tab bar
+
+-- window
 config.adjust_window_size_when_changing_font_size = false
-config.use_fancy_tab_bar = true
 config.window_background_opacity = 0.3
-config.macos_window_background_blur = 20
 config.window_decorations = "RESIZE"
 config.window_padding = {
 	left = 0,
@@ -23,23 +23,29 @@ config.window_padding = {
 	top = 0,
 	bottom = 0
 }
+
+-- COLORS
+config.color_scheme = 'Monokai Pro Ristretto (Gogh)'
 config.colors = {
   tab_bar = {
     background = 'rgba(0,0,0,0)'
   }
 }
-
-config.unix_domains = {
-  {
-    name = 'unix',
-  },
-}
+-- TAB BAR 
+config.hide_tab_bar_if_only_one_tab = true
+config.use_fancy_tab_bar = false
+config.macos_window_background_blur = 20
+config.show_new_tab_button_in_tab_bar = false
 
 -- This causes `wezterm` to act as though it was started as
 -- `wezterm connect unix` by default, connecting to the unix
 -- domain on startup.
 -- If you prefer to connect manually, leave out this line.
+config.unix_domains = {
+  {
+    name = 'unix',
+  },
+}
 config.default_gui_startup_args = { 'connect', 'unix' }
 
-config.color_scheme = 'Monokai Pro Ristretto (Gogh)'
 return config
