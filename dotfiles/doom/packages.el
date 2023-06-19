8f674ddcac5bad6942eacef65521dbb13eba306c
Philippe Latour <drawer@darwin-devel.local>
2023-06-19 02:25:59 -0400

    Removed annalist and added jupyter to doom emacs

--- dotfiles/doom/packages.el
+++ dotfiles/doom/packages.el
@@ -11,8 +11,7 @@
 (package! haskell-mode)
 (package! ein)
 (package! org-modern)
-(package! annalist)
-;;(package! jupyter)
+(package! jupyter)
 
 ;; To install a package directly from a remote git repo, you must specify a
 ;; `:recipe'. You'll find documentation on what `:recipe' accepts here:
